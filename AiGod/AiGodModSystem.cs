f647a64734ad84167c687ccef7864bfa73b59fff
Christian Lewis <christianclewis@gmail.com>
2025-05-12 09:49:58 -0400

    Add project files.

---
+++ AiGod/AiGodModSystem.cs
@@ -0,0 +1,30 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.Server;
+
+namespace AiGod
+{
+    public class AiGodModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from God on the server side: " + Lang.Get("aigod:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from God on the mod client side: " + Lang.Get("aigod:hello"));
+        }
+
+    }
+}
